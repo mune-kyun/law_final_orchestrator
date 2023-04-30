990f018a5b2887de01b1515e26dbf2424c102f3b
mune-kyun <rzknurfaizi@gmail.com>
2023-04-30 10:33:06 +0700

    cors

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: ['http://localhost:3000', 'http://34.172.160.15'],
+    methods: ['POST', 'PUT', 'DELETE', 'GET'],
+  });
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3000);
+  await app.listen(8080);
 }
 bootstrap();
